35b777199013f4ee39f54b5113ba7a0794d46746
Steven Nguyen <steyen@umich.edu>
2020-09-10 14:48:46 -0400

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Steven Nguyen on 9/10/20.
+//  Copyright © 2020 XenoStorm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
